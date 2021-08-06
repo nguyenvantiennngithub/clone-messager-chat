17b119893b597297f33646675c31c4c65f054362
nguyenvantiennngithub <nguyenvantiennn0910@gmail.com>
2021-08-06 15:30:54 +0700

    heroku

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ console.log(redisURL.port, redisURL.hostname)
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
-client.on('connect', function(){
-    console.log("Connect redis")
-})
+// client.on('connect', function(){
+//     console.log("Connect redis")
+// })
 
 app.use(express.static('./public'))
 app.set('view engine', 'ejs')
