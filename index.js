c6bab3a4d0c465ec77eadd7513a16e3dbaeec7ba
nguyenvantiennngithub <nguyenvantiennn0910@gmail.com>
2021-08-06 15:29:32 +0700

    heroku

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(process.env.REDIS_TLS_URL)
 
 
 var redisURL = url.parse(process.env.REDIS_TLS_URL);
+console.log(redisURL.port, redisURL.hostname)
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
